c203e1f982c2e3732532d80a3f989514d4de44a4
edy-jiang <73779440+edy-jiang@users.noreply.github.com>
2020-11-01 20:08:16 +0800

    RNN_initialize
    demo to build RNN

---
+++ rnn_demo.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+import torch
+
+# 实例化RNN对象
+# 第一个参数：input_size(输入张量x的维度)
+rnn = nn.RNN(5, 6, 2)
+input = torch.randn(1, 3, 5)
+h0 = torch.randn(2, 3, 6)
+output, hn = rnn(input,h0)
+print(output.shape)
+print(hn.shape)
